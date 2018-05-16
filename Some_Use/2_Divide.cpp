17716307860f73b2fe8f5ee3c4c6a0bed75ed58c
LWJ9527 <2311349863@qq.com>
2018-05-16 19:39:59 +0800

    no

--- Some_Use/2_Divide.cpp
+++ Some_Use/2_Divide.cpp
@@ -6,7 +6,7 @@
 #include<vector>
 #include<algorithm>
 
-const int MAX = 1000000;
+const int MAX = 5;
 int Search(std::vector<int>& ,int);
 
 int main()
@@ -14,7 +14,12 @@ int main()
 	int Number;
 	srand(unsigned(time(NULL)));
 	std::vector<int> Temp;
-	for(int i = 0;i < MAX;i++) Temp.push_back(rand());
+	for(int i = 0;i < MAX;i++)
+	{
+		int a; std::cin >> a;
+		Temp.push_back(a);
+		//Temp.push_back(rand());
+	}
 	sort(Temp.begin(),Temp.end());
 //	for(auto temp : Temp) std::cout << temp << '\n';
 	while(std::cin >> Number)
@@ -24,10 +29,11 @@ int main()
 
 int Search(std::vector<int>& p,int number)
 {
+	int mid;
 	int front = 0,end = p.size()-1;
 	while(front <= end)
 	{
-		int mid = (front+end) / 2;
+		mid = (front+end) / 2;
 		if(p[mid] == number) return mid+1;
 		if(p[mid] > number) end = mid-1;
 		if(p[mid] < number) front = mid+1;
