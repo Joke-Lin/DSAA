a1371354f88c92dc8f7a1c44c187940cbcc26a8d
LWJ9527 <2311349863@qq.com>
2018-07-15 19:05:26 +0800

    POJ3368RE

--- 江安的七月/BIT_RMQ/Frequent_values_RE.cpp
+++ 江安的七月/BIT_RMQ/Frequent_values_RE.cpp
@@ -14,10 +14,11 @@ int n,q;
 int quene_pos[maxn];//存正数
 int quene_neg[maxn];//存负数
 int num[maxn];
+int index[maxn];
 
 int main()
 {
-    freopen("./in.txt","r",stdin);
+    // freopen("./in.txt","r",stdin);
     while(scanf("%d",&n))
     {
         if(n == 0) break;
@@ -26,25 +27,29 @@ int main()
         for(int i = 1;i <= n;i++) scanf("%d",num+i);
         while(q--)
         {
-            CLS(quene_pos); CLS(quene_neg);
+            CLS(quene_pos); CLS(quene_neg);CLS(index);
             int x,y;
+            int sum = 0;
             scanf("%d%d",&x,&y);
             for(int i = x;i <= y;i++)
             {
                 if(num[i]>= 0) quene_pos[num[i]]++;
                 else quene_neg[abs(num[i])]++;
+                index[sum] = abs(num[i]);
+                sum++;
             }
+           // printf("sum:%d\n",sum);
             int ans = 0;
-            for(int i = 0;i < maxn;i++)
+            for(int i = 0;i < sum;i++)
             {
                 //if(quene_neg[i]==0 && quene_neg[i]==0) continue;
-                ans = max(ans,quene_pos[i]);
-                ans = max(ans,quene_neg[i]);
+                ans = max(ans,quene_pos[index[i]]);
+                ans = max(ans,quene_neg[index[i]]);
             }
             printf("%d\n",ans);
         }
     }
-    freopen("CON","r",stdin);
-    system("pause");
+    // freopen("CON","r",stdin);
+    // system("pause");
     return 0;
 }
