beb28650e33a085d3e1dc4e3b039fce991cf65f5
LWJ9527 <2311349863@qq.com>
2018-08-02 09:28:39 +0800

    up date

--- 江安的七月/数论/flog_date.cpp
+++ 江安的七月/数论/flog_date.cpp
@@ -1,43 +1,44 @@
-//https://blog.csdn.net/dtl66/article/details/52079784
-#include<stdio.h>
-#include<math.h>
-
-int x,y;
-int exgcd(int a,int b)//方程ax+by=gcd(a,b)的一对解 
-{
-    if(b==0)
-    {
-        x=1;
-        y=0;
+//https://www.cnblogs.com/H-Vking/p/4104794.html
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <algorithm>
+#include <cmath>
+using namespace std;  
+long long extend_gcd(long long a,long long b,long long &x,long long &y)
+{    if(b==0){
+        x=1;y=0;
         return a;
-    }
-    int r=exgcd(b,a%b);
-    int tem=x;
-    x=y;
-    y=tem-a/b*y;
-    return r; 
-} 
-
+    }else{
+        long long r=extend_gcd(b,a%b,y,x);
+        y-=x*(a/b);
+        return r;
+    }    
+}
 int main()
 {
-    int m,n,X,Y,l,a,b,c,d; 
-    while(~scanf("%d %d %d %d %d",&X,&Y,&m,&n,&l))
-    {
-        a=n-m;
-        b=l;
-        c=X-Y;
-        d=exgcd(a,b);
-        if(c%d!=0)//此条件下，无整数解 c与a,b没有约数
-        {
-            printf("Impossible\n");
-            continue;
+	long long x,y,m,n,L;
+    long long a,b,c,gcd;
+    while(scanf("%lld %lld %lld %lld %lld",&x,&y,&m,&n,&L)!=EOF)
+    {    a=m-n;
+        b=L;
+        c=y-x;
+        if(a<0)
+        {    a=-a;
+            c=-c;
         }
-        x=x/d*c;//ax+by=c的x的一个特解 
-        int b1=abs(b/d);//ax+by=c的通解x=x1+b1*t(t为任意整数） 特解+通解
-        x=(x%b1+b1)%b1;//确定最小正解 
-
-        printf("%d\n",x);
-
-     } 
+        gcd=extend_gcd(a,b,x,y);
+        if(c%gcd!=0)    
+            cout<<"Impossible"<<endl;
+        else{    
+            x=x*c/gcd;
+            int t=b/gcd;
+            if(x>=0)
+                x=x%t;
+            else
+                x=x%t+t;
+            cout<<x<<endl;
+        }
+    }
     return 0;
- } 
+}
